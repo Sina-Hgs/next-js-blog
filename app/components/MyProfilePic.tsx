93685bc0a6994a92b88e540c574b1810f290f291
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-09-18 20:37:16 +0330

    first commit

---
+++ app/components/MyProfilePic.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+function MyProfilePic() {
+  return (
+    <section className="w-full mx-auto">
+      <Image />
+    </section>
+  );
+}
+
+export default MyProfilePic;
