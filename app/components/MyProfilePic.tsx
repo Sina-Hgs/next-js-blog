e45c0224e7b185746b85d226bb684b602c7bcc1a
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-09-19 18:02:40 +0330

    added posts

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 function MyProfilePic() {
   return (
     <section className="w-full mx-auto">
-      <Image />
+      <Image
+        className="border-4 border-black
+        dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+        src="/images/photo_2023-02-09_17-36-23 (2).jpg"
+        width={200}
+        height={200}
+        alt="Sina Haghshenas"
+        priority={true}
+      />
     </section>
   );
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-        <Link href="/" className="text-white/90 no-underline hover:text-white">
-          Sina Haghshenas
-        </Link>
+        <h1 className="text-2xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+          <Link
+            href="/"
+            className="text-white/90 no-underline hover:text-white"
+          >
+            Sina Haghshenas
+          </Link>
+        </h1>
+
+        <div className="flex flex-row justify-center items-center text-white/90 hover:text-white">
+          <Link
+            href="https://github.com/Sina-Hgs"
+            className="text-inherit text-3xl hover:scale-110 transition-all"
+          >
+            <FaGithub />
+          </Link>
+        </div>
       </div>
     </nav>
   );
