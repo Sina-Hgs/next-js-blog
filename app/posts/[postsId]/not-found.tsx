7ee0096bb5c9c7df52681ebd8ff072cae3167a03
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-09-20 18:08:13 +0330

    working on dynamic routes

---
+++ app/posts/[postsId]/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <h1 className="flex flex-row justify-center mt-5 text-xl text-center ">
+      Seems like you're lost!
+      <br />
+      The requested page does not exist 👀.
+    </h1>
+  );
+};
+
+export default NotFound;
