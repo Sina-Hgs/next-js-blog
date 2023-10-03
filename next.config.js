1b07671da60a89a00d06a8f1c8aebdde40c2f1fe
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-10-03 17:16:30 +0330

    fixing config for github pages

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
 
+  basePath: isProd ? "https://sina-hgs.github.io/next-js-blog/" : undefined,
   assetPrefix: isProd ? "https://sina-hgs.github.io/next-js-blog/" : undefined,
 
   images: {
     unoptimized: true,
+    loader: "akamai",
+    path: "https://sina-hgs.github.io/next-js-blog/",
   },
 };
 
