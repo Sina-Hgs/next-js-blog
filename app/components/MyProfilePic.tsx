c351823be5e00d181396627df53676a0bc55b229
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-10-03 17:58:54 +0330

    changed the path

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
+import profilePicture from "../../public/images/photo_2023-02-09_17-36-23 (2).jpg";
+
 function MyProfilePic() {
   return (
     <section className="w-full mx-auto">
       <Image
         className="border-4 border-black
         dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/images/photo_2023-02-09_17-36-23 (2).jpg"
+        src={profilePicture}
         width={200}
         height={200}
         alt="Sina Haghshenas"
