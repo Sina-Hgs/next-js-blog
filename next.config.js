f2989b4ba62f6d472c2c7427c2345c54e82beb46
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-10-03 17:09:17 +0330

    fixed the asset prefix for hosting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
 
+  assetPrefix: isProd ? "https://sina-hgs.github.io/next-js-blog/" : undefined,
+
   images: {
     unoptimized: true,
-    loader: "akamai",
-    path: "https://sina-hgs.github.io/next-js-blog/",
   },
 };
 
