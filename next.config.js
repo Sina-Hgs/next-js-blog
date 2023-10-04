9d222ae1a049782b04a2e844f9509b688e26c8ee
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-10-04 12:26:53 +0330

    working on deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/next-js-blog",
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
